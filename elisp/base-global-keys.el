a74f545106be3831a088b2e2f26c1ab5b77763c1
Ivin Sony <ivin.sony@netscout.com>
2017-08-03 18:14:02 +0100

    tabruller added

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -129,11 +129,32 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-  
-(defun-prefix-alt shk-tabbar-next (tabbar-forward-tab) (tabbar-forward-group) (tabbar-mode 1))
-(defun-prefix-alt shk-tabbar-prev (tabbar-backward-tab) (tabbar-backward-group) (tabbar-mode 1))
- (global-set-key [(control tab)] 'shk-tabbar-next)
- (global-set-key [(control shift tab)] 'shk-tabbar-prev)
+
+
+(dolist (func '(tabbar-mode tabbar-forward-tab tabbar-forward-group tabbar-backward-tab tabbar-backward-group))
+      (autoload func "tabbar" "Tabs at the top of buffers and easy control-tab navigation"))
+    
+    (defmacro defun-prefix-alt (name on-no-prefix on-prefix &optional do-always)
+      `(defun ,name (arg)
+         (interactive "P")
+         ,do-always
+         (if (equal nil arg)
+             ,on-no-prefix
+           ,on-prefix)))
+    
+    (defun-prefix-alt shk-tabbar-next (tabbar-forward-tab) (tabbar-forward-group) (tabbar-mode 1))
+    (defun-prefix-alt shk-tabbar-prev (tabbar-backward-tab) (tabbar-backward-group) (tabbar-mode 1))
+    
+(global-set-key [(control tab)] 'shk-tabbar-next)
+(global-set-key [(control shift tab)] 'shk-tabbar-prev)
+
+(setq tabbar-ruler-global-tabbar t) ; If you want tabbar
+(setq tabbar-ruler-global-ruler t) ; if you want a global ruler
+(setq tabbar-ruler-popup-menu t) ; If you want a popup menu.
+(setq tabbar-ruler-popup-toolbar t) ; If you want a popup toolbar
+(setq tabbar-ruler-popup-scrollbar t) ; If you want to only show the
+                                        ; scroll bar when your mouse is moving.
+(require 'tabbar-ruler)
 
 
 ;; key chords
