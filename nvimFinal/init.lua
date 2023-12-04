3e4c28cf1cc8fc1158d9aaae36aaa69b99b7c3a3
Ivin Sony <ivinpolosony@gmail.com>
2023-12-04 16:19:24 +0000

    Update metals with deps

--- nvimFinal/init.lua
+++ nvimFinal/init.lua
@@ -1,63 +1,56 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.termguicolors = true
-local python = io.popen('zsh -c \'which python3\'')
+local python = io.popen("zsh -c 'which python3'")
 if python then
-  vim.g.python_host_prog = python:read()
-  python:close()
+	vim.g.python_host_prog = python:read()
+	python:close()
 end
-
 
 require("core.lazy")
 require("core.opts")
 require("core.keymaps")
 -- vim.cmd.colorscheme("ayu")
 
-
 local neodev = vim.F.npcall(require, "neodev")
 if neodev then
-  neodev.setup {
-    override = function(_, library)
-      library.enabled = true
-      library.plugins = true
-    end,
-    lspconfig = true,
-    pathStrict = true,
-  }
+	neodev.setup({
+		override = function(_, library)
+			library.enabled = true
+			library.plugins = true
+		end,
+		lspconfig = true,
+		pathStrict = true,
+	})
 end
 
-
-
-
-
 local attach_to_buffer = function(output_bufnr, command)
-  vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup("runonsave", { clear = true }),
-    pattern = "*.py",
-    callback = function()
-      local currentFile = vim.api.nvim_buf_get_name(0)
-      local appendData = function(_, data)
-        if data then
-          vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
-        end
-      end
-      vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { "output:" })
-      vim.fn.jobstart({ "python3", currentFile }, {
-        stdout_buffered = true,
-        on_stdout = appendData,
-        on_stderr = appendData,
-      })
-    end,
-  })
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = vim.api.nvim_create_augroup("runonsave", { clear = true }),
+		pattern = "*.py",
+		callback = function()
+			local currentFile = vim.api.nvim_buf_get_name(0)
+			local appendData = function(_, data)
+				if data then
+					vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+				end
+			end
+			vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { "output:" })
+			vim.fn.jobstart({ "python3", currentFile }, {
+				stdout_buffered = true,
+				on_stdout = appendData,
+				on_stderr = appendData,
+			})
+		end,
+	})
 end
 
 attach_to_buffer(192, {})
 
-vim.api.nvim_create_user_command("Autorun", function ()
-  print("auto run...")
-  local bufnr = vim.fn.input "Bufnr: "
-  local pattern = vim.fn.input "Pattern: "
-  local command = vim.split(vim.fn.input "Command to run the file: like python","")
-  attach_to_buffer(tonumber(bufnr),command)
-end
-)
+vim.api.nvim_create_user_command("Autorun", function()
+	print("auto run...")
+	local bufnr = vim.fn.input("Bufnr: ")
+	local pattern = vim.fn.input("Pattern: ")
+	local command = vim.split(vim.fn.input("Command to run the file: like python"), "")
+	attach_to_buffer(tonumber(bufnr), command)
+end, {})
--- nvimFinal/lua/plugins/metals.lua
+++ nvimFinal/lua/plugins/metals.lua
@@ -1,84 +1,95 @@
 return {
-    {
-      "scalameta/nvim-metals",
-      dependencies = { 
-        "nvim-lua/plenary.nvim",
-        "mfussenegger/nvim-dap",
-      },
-      ft = { "scala", "sbt", "java" },
-      keys = {
-        {'<leader>me', function() require "telescope".extensions.metals.commands() end, desc="Metals commands"},
-        {'<leader>mc', function() require "metals".compile_cascade() end, desc="Metals compile cascade"},
-      },
-      config = function()
-        local metals = require("metals")
-        local config = metals.bare_config()
-  
-        config.settings = {
-          showImplicitArguments = true,
-          excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-          serverProperties = { "-Xmx2g" },
-          enableSemanticHighlighting = true,
-        }
-        config.init_options.statusBarProvider = "on"
-        -- cmp integration
-        config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-  
-        local dap = require("dap")
-        dap.configurations.scala = {
-          {
-            type = "scala",
-            request = "launch",
-            name = "RunOrTest",
-            metals = {
-              runType = "runOrTestFile",
-            },
-          },
-        }
-        dap.listeners.after["event_terminated"]["nvim-metals"] = function()
-          -- vim.notify("Tests have finished!")
-          dap.repl.open()
-        end
-  
-        config.on_attach = function(client, bufnr)
-          metals.setup_dap()
-          require("lsp-format").on_attach(client, bufnr)
-        end
-  
-        -- Autocmd that will actually be in charge of starting the whole thing
-        local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-        vim.api.nvim_create_autocmd("FileType", {
-          pattern = { "scala", "sbt", "java" },
-          callback = function()
-            metals.initialize_or_attach(config)
-          end,
-          group = nvim_metals_group,
-        })
+	{
+		"scalameta/nvim-metals",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"mfussenegger/nvim-dap",
+			"lukas-reineke/lsp-format.nvim",
+		},
+		ft = { "scala", "sbt", "java" },
+		keys = {
+			{
+				"<leader>me",
+				function()
+					require("telescope").extensions.metals.commands()
+				end,
+				desc = "Metals commands",
+			},
+			{
+				"<leader>mc",
+				function()
+					require("metals").compile_cascade()
+				end,
+				desc = "Metals compile cascade",
+			},
+		},
+		config = function()
+			local metals = require("metals")
+			local config = metals.bare_config()
 
+			config.settings = {
+				showImplicitArguments = true,
+				excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+				serverProperties = { "-Xmx2g" },
+				enableSemanticHighlighting = true,
+			}
+			config.init_options.statusBarProvider = "on"
+			-- cmp integration
+			config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        -- local metals_status = function() 
-        --     return vim.g['metals_status']
-        -- end
+			local dap = require("dap")
+			dap.configurations.scala = {
+				{
+					type = "scala",
+					request = "launch",
+					name = "RunOrTest",
+					metals = {
+						runType = "runOrTestFile",
+					},
+				},
+			}
+			dap.listeners.after["event_terminated"]["nvim-metals"] = function()
+				-- vim.notify("Tests have finished!")
+				dap.repl.open()
+			end
 
-    
-        -- require("lualine").setup({
-        --     options = { 
-        --       theme = "auto",
-        --       disabled_filetypes = {},
-        --     },
-        --     sections = {
-        --       lualine_c = {
-        --         { metals_status, cond = function() return vim.g['metals_status'] ~= nil end }
-        --       },
-        --       lualine_x = {
-        --         { "filetype", icon_only = true },
-        --               "fileformat",
-        --               "filesize",
-        --           },
-        --       lualine_y = {'progress'},
-        --       lualine_z = {'location'}
-        --     }
-        --   })
-      end,
-    },
-  }
+			config.on_attach = function(client, bufnr)
+				metals.setup_dap()
+				require("lsp-format").on_attach(client, bufnr)
+			end
+
+			-- Autocmd that will actually be in charge of starting the whole thing
+			local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "scala", "sbt", "java" },
+				callback = function()
+					metals.initialize_or_attach(config)
+				end,
+				group = nvim_metals_group,
+			})
+
+			-- local metals_status = function()
+			--     return vim.g['metals_status']
+			-- end
+
+			-- require("lualine").setup({
+			--     options = {
+			--       theme = "auto",
+			--       disabled_filetypes = {},
+			--     },
+			--     sections = {
+			--       lualine_c = {
+			--         { metals_status, cond = function() return vim.g['metals_status'] ~= nil end }
+			--       },
+			--       lualine_x = {
+			--         { "filetype", icon_only = true },
+			--               "fileformat",
+			--               "filesize",
+			--           },
+			--       lualine_y = {'progress'},
+			--       lualine_z = {'location'}
+			--     }
+			--   })
+		end,
+	},
+}
