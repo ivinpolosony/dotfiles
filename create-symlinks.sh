c0367071232253cb96a942c58138e3b783f21d24
Ivin Sony <ivinpolosony@gmail.com>
2023-12-15 13:02:01 +0000

    hammer

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -10,3 +10,8 @@ folder_path="~/Library/KeyBindings"
 mkdir -p $folder_path
 ## credit https://gist.githubusercontent.com/jwreagor/9670905/raw/1c1cd2e84daf07c9a4c8de0ff86d1baf75d858c6/EmacsKeyBinding.dict
 ln -s $PWD/EmacsKeyBinding.dict $folder_path/DefaultKeyBinding.dict
+
+hammerspoon_path="~/.hammerspoon"
+mkdir -p $hammerspoon_path
+ln -s $PWD/hammer/init.lua ~/.hammerspoon/init.lua
+
--- hammer/init.lua
+++ hammer/init.lua
@@ -1,15 +1,9 @@
-
 hs.hotkey.bind({"ctrl","alt"},"1", function()
-  hs.application.launchOrFocus("Sublime Text")
+  hs.application.launchOrFocus("kitty")
 end)
-
-
 
 hs.hotkey.bind({"ctrl","alt"},"2", function()
   hs.application.launchOrFocus("Google Chrome Dev")
 end)
 
 
-hs.hotkey.bind({"ctrl","alt"},"3", function()
-  hs.application.launchOrFocus("kitty")
-end)
