d699d37847f14a292d3a5ed97a2b065dbd69943f
Ivin Sony <ivinpolosony@gmail.com>
2024-10-26 14:59:44 +0100

    added keymaps

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,106 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- vim.api.nvim_set_keymap("n", "<leader>l", ":bnext<enter>", { desc = "Lazy" })
+vim.api.nvim_set_keymap("n", "<leader><TAB>", ":b#<CR>", {desc = "Lazy"})
+vim.api.nvim_set_keymap("n", "<Leader>fs", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<M-d>", "dw", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<M-w>", "<C-Space>", { noremap = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "<F8>", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- document existing key chains
+require("which-key").register(
+    {
+	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+	["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+	["<leader>h"] = { name = "More git", _ = "which_key_ignore" },
+	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+    }
+)
+
+-- In this case, we create a function that lets us more easily define mappings specific
+-- for LSP related items. It sets the mode, buffer and description for us each time.
+local nmap = function(keys, func, desc)
+	if desc then
+		desc = "LSP: " .. desc
+	end
+
+	vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+end
+
+nmap("<S-F6>", vim.lsp.buf.rename, "[R]e[n]ame")
+nmap("<M-CR>", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+
+nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+-- See `:help K` for why this keymap
+nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+-- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+-- Lesser used LSP functionality
+nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+nmap("<leader>wl", function()
+	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, "[W]orkspace [L]ist Folders")
+
+-- Create a command `:Format` local to the LSP buffer
+--   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+--     vim.lsp.buf.format()
+--   end, { desc = 'Format current buffer with LSP' })
+
+vim.keymap.set("n", "<leader>fr", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>ss", function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
+end, { desc = "[/] Fuzzily search in current buffer" })
+
+vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>/", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+
+
+
+
+
+-- GO test closset 
+-- vim.keymap.set("n", "<leader>tt", ":GoTest -n <cr>", { desc = "Run clossest test" })
+-- vim.keymap.set("n", "<leader>r", ":GoRun -F<cr>", { desc = "Run"  })
+-- vim.keymap.set("n", "<leader>r", ":GoRun <cr>", { desc = "Run"  })
+-- vim.keymap.set("n", "<leader>qt", ":GoTermClose<cr>", { desc = "Run"  })
+vim.keymap.set("n", "<leader>l", ":lua vim.diagnostic.open_float()<cr>", { desc = "diag float"  })
+
+
+
+
+
+
+
